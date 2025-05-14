bdfd3f7c93cf3db101a0b842904af866ee9aaa36
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-14 23:30:21 +0200

    media display for PriceList

--- src/components/Content/Cruises/PriceList/index.js
+++ src/components/Content/Cruises/PriceList/index.js
@@ -3,78 +3,40 @@ import { StyledTable, TableHeader, TableCell, TableRow } from "./styled";
 
 const PriceList = () => (
     <Wrapper>
-        <Header>
-            Cennik
-        </Header>
+        <Header>Cennik</Header>
         <StyledTable>
             <thead>
                 <TableRow>
                     <TableHeader></TableHeader>
                     <TableHeader></TableHeader>
-                    <TableHeader>
-                        Bilet normalny
-                    </TableHeader>
-                    <TableHeader>
-                        Bilet ulgowy do lat 10
-                    </TableHeader>
+                    <TableHeader>Bilet normalny</TableHeader>
+                    <TableHeader>Bilet ulgowy do lat 10</TableHeader>
                 </TableRow>
             </thead>
             <tbody>
                 <TableRow>
-                    <TableCell>
-                        Rejs w jedną stronę
-                    </TableCell>
-                    <TableCell>
-                        ➡️
-                    </TableCell>
-                    <TableCell>
-                        17 PLN
-                    </TableCell>
-                    <TableCell>
-                        14 PLN
-                    </TableCell>
+                    <TableCell data-label="Typ rejsu">Rejs w jedną stronę</TableCell>
+                    <TableCell data-label="Ikona">➡️</TableCell>
+                    <TableCell data-label="Bilet normalny">17 PLN</TableCell>
+                    <TableCell data-label="Bilet ulgowy">14 PLN</TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell>
-                        Rejs w dwie strony
-                    </TableCell>
-                    <TableCell>
-                        ↔️
-                    </TableCell>
-                    <TableCell>
-                        34 PLN
-                    </TableCell>
-                    <TableCell>
-                        28 PLN
-                    </TableCell>
+                    <TableCell data-label="Typ rejsu">Rejs w dwie strony</TableCell>
+                    <TableCell data-label="Ikona">↔️</TableCell>
+                    <TableCell data-label="Bilet normalny">34 PLN</TableCell>
+                    <TableCell data-label="Bilet ulgowy">28 PLN</TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell>
-                        Rejs widokowy dookoła jeziora
-                    </TableCell>
-                    <TableCell>
-                        🔄️
-                    </TableCell>
-                    <TableCell>
-                        35 PLN
-                    </TableCell>
-                    <TableCell>
-                        28 PLN
-                    </TableCell>
+                    <TableCell data-label="Typ rejsu">Rejs widokowy dookoła jeziora</TableCell>
+                    <TableCell data-label="Ikona">🔄️</TableCell>
+                    <TableCell data-label="Bilet normalny">35 PLN</TableCell>
+                    <TableCell data-label="Bilet ulgowy">28 PLN</TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell>
-                        Pasażer z rowerem
-                    </TableCell>
-                    <TableCell>
-                        🚵
-                    </TableCell>
-                    <TableCell>
-                        27 PLN
-                    </TableCell>
-                    <TableCell>
-                        Dzieci do lat 4 <strong>bezpłatnie</strong>
-                    </TableCell>
+                    <TableCell data-label="Typ rejsu">Pasażer z rowerem</TableCell>
+                    <TableCell data-label="Ikona">🚵</TableCell>
+                    <TableCell data-label="Bilet normalny">27 PLN</TableCell>
+                    <TableCell data-label="Bilet ulgowy">Dzieci do lat 4 <strong>bezpłatnie</strong></TableCell>
                 </TableRow>
             </tbody>
         </StyledTable>
--- src/components/Content/Cruises/PriceList/styled.js
+++ src/components/Content/Cruises/PriceList/styled.js
@@ -4,6 +4,18 @@ export const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
   text-align: center;
+
+  @media (max-width: 768px) {
+    border: 0;
+
+    thead {
+      display: none;
+    }
+
+    tbody {
+      display: block;
+    }
+  }
 `;
 
 export const TableHeader = styled.th`
@@ -14,8 +26,33 @@ export const TableHeader = styled.th`
 
 export const TableRow = styled.tr`
   border-top: 1px solid #ddd;
+
+  @media (max-width: 768px) {
+    display: block;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 10px;
+  }
 `;
 
 export const TableCell = styled.td`
   padding: 16px;
-  `;
+
+  @media (max-width: 768px) {
+    display: block;
+    text-align: right;
+    position: relative;
+    padding-left: 50%;
+
+    &::before {
+      content: attr(data-label);
+      position: absolute;
+      left: 16px;
+      width: 45%;
+      text-align: left;
+      font-weight: bold;
+      white-space: nowrap;
+    }
+  }
+`;
