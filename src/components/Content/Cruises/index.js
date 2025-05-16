ce95b9403d4a5b5a745b923045d7dc751597b639
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-16 13:57:24 +0200

    changing offset for links and deleting br

--- src/components/Content/Cruises/index.js
+++ src/components/Content/Cruises/index.js
@@ -14,11 +14,11 @@ const Cruises = () => (
         <Header className="mobile-header">Rejsy gondolą po jeziorze Czorsztyńskim</Header>
         <DescriptionSection>
             <Description>
-                Oferujemy jedyną w swoim rodzaju i niepowtarzalną możliwość rejsu gondolami pomiędzy naszymi przystaniami na dwóch trasach. <br />
-                Trasa łącząca ruiny zamków w Czorsztynie i Niedzicy, kurs stały, odbywa się co 15 minut z Czorsztyna lub Niedzicy. To idealna okazja swobodnego zwiedzenia okolicy na obydwu brzegach Jeziora Czorsztyńskiego. <br />
-                Trasa dookoła Jeziora Czorsztyńskiego, rejs rusza z minimum 8 osobami na pokładzie gondoli, czas rejsu to około 40 minut, daje możliwość zapoznania się z malowniczymi zatoczkami i plażami jeziora, rejsy odbywają się po uzgodnieniu telefonicznym w godzinach porannych (9:00-10:00) popołudniowych (17:00-18:00) w sezonie (1 lipca -31 sierpnia). <br />
-                Nasze gondole “Czorsztynianki” kursują codziennie, od 1 maja do 30 września, w godzinach 9.00 – 18.00 w sezonie (od 1 lipca do 31 sierpnia) oraz 10.00 – 16.00 poza sezonem (od 1 maj do 30 czerwca, od 1 września do 30 września). Dysponujemy 6 gondolami, które jednorazowo mogą zabrać 72 osoby. Na gondolach można przewozić rowery, zwierzęta są mile widziane. <br />
-                Po rejsie zapraszamy do naszej przystani. Przystań Jędruś w Czorsztynie oferuje wiele możliwości spędzenia czasu nad urokliwym Jeziorem Czorsztyńskim. <br />
+                Oferujemy jedyną w swoim rodzaju i niepowtarzalną możliwość rejsu gondolami pomiędzy naszymi przystaniami na dwóch trasach.
+                Trasa łącząca ruiny zamków w Czorsztynie i Niedzicy, kurs stały, odbywa się co 15 minut z Czorsztyna lub Niedzicy. To idealna okazja swobodnego zwiedzenia okolicy na obydwu brzegach Jeziora Czorsztyńskiego.
+                Trasa dookoła Jeziora Czorsztyńskiego, rejs rusza z minimum 8 osobami na pokładzie gondoli, czas rejsu to około 40 minut, daje możliwość zapoznania się z malowniczymi zatoczkami i plażami jeziora, rejsy odbywają się po uzgodnieniu telefonicznym w godzinach porannych (9:00-10:00) popołudniowych (17:00-18:00) w sezonie (1 lipca -31 sierpnia).
+                Nasze gondole “Czorsztynianki” kursują codziennie, od 1 maja do 30 września, w godzinach 9.00 – 18.00 w sezonie (od 1 lipca do 31 sierpnia) oraz 10.00 – 16.00 poza sezonem (od 1 maj do 30 czerwca, od 1 września do 30 września). Dysponujemy 6 gondolami, które jednorazowo mogą zabrać 72 osoby. Na gondolach można przewozić rowery, zwierzęta są mile widziane.<br />
+                Po rejsie zapraszamy do naszej przystani. Przystań Jędruś w Czorsztynie oferuje wiele możliwości spędzenia czasu nad urokliwym Jeziorem Czorsztyńskim. 
                 W Przystani znajduje się także mała gastronomia, gdzie można zakupić zimne i ciepłe napoje, przekąski, lody, słodycze. Do dyspozycji gości oddajemy ogródek z ławkami, parasolami chroniącymi przed słońcem i deszczem, wygodne pufy i leżaki oraz hamaki w zacienionym zagajniku.
             </Description>
         </DescriptionSection>
