9cb251b70d86bab29cb766c3b73a3c9807932931
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-17 15:20:37 +0200

    adapting navbar to mobile devices

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,27 +1,64 @@
-import { Navbar, NavItem, NavLinks, NavLogo, NavWrapper } from "./styled";
+import { useState } from "react";
+import { Burger, MobileMenu, Navbar, NavItem, NavLinks, NavLogo, NavWrapper } from "./styled";
 import Logo from "./logo_CMYK_kolor.png"
 import { Link } from "react-scroll";
 
-const NavigationBar = () => (
-    <Navbar>
-        <NavWrapper>
-            <Link to="home" smooth={true} duration={600} offset={-90}>
-                <NavLogo src={Logo} alt="Logo" />
-            </Link>
-            <NavLinks>
-                <NavItem to="usługi" smooth={true} duration={600} offset={-90}>
-                    Oferta
-                </NavItem>
-                <NavItem to="cennik-rejsu" smooth={true} duration={800} offset={-90}>
-                    Cennik rejsów
-                </NavItem>
-                <NavItem to="cennik-wypozyczalni" smooth={true} duration={1200} offset={-90}>
-                    Cennik wypożyczalni
-                </NavItem>
+const NavigationBar = () => {
 
-            </NavLinks>
-        </NavWrapper>
-    </Navbar>
-);
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Navbar>
+            <NavWrapper>
+                <Link to="home" smooth={true} duration={600} offset={-90}>
+                    <NavLogo src={Logo} alt="Logo" />
+                </Link>
+
+                <Burger onClick={() => setIsOpen(!isOpen)}>
+                    <div />
+                    <div />
+                    <div />
+
+                </Burger>
+
+                <NavLinks>
+                    <NavItem to="usługi" smooth={true} duration={600} offset={-90}>
+                        Oferta
+                    </NavItem>
+                    <NavItem to="cennik-rejsu" smooth={true} duration={800} offset={-90}>
+                        Cennik rejsów
+                    </NavItem>
+                    <NavItem to="cennik-wypozyczalni" smooth={true} duration={1200} offset={-90}>
+                        Cennik wypożyczalni
+                    </NavItem>
+                    <NavItem>
+                        Partnerzy
+                    </NavItem>
+                    <NavItem>
+                        Dojazd i kontakt
+                    </NavItem>
+                </NavLinks>
+
+                <MobileMenu $open={isOpen}>
+                    <NavItem to="usługi" smooth={true} duration={600} offset={-90}>
+                        Oferta
+                    </NavItem>
+                    <NavItem to="cennik-rejsu" smooth={true} duration={800} offset={-90}>
+                        Cennik rejsów
+                    </NavItem>
+                    <NavItem to="cennik-wypozyczalni" smooth={true} duration={1200} offset={-90}>
+                        Cennik wypożyczalni
+                    </NavItem>
+                    <NavItem>
+                        Partnerzy
+                    </NavItem>
+                    <NavItem>
+                        Dojazd i kontakt
+                    </NavItem>
+                </MobileMenu>
+            </NavWrapper>
+        </Navbar>
+    );
+};
 
 export default NavigationBar;
--- src/components/NavigationBar/styled.js
+++ src/components/NavigationBar/styled.js
@@ -30,12 +30,17 @@ export const NavLogo = styled.img`
     height: 80px;
     width: 80px;
     cursor: pointer;
-    margin: 0;
+    margin-top: 4px;
 `;
 
 export const NavLinks = styled.div`
   display: flex;
-  gap: 20px;
+  gap: 30px;
+  margin-left: 20px;
+
+   @media (max-width: 767px) {
+        display: none;
+  }
 `;
 
 export const NavItem = styled(Link)`
@@ -44,10 +49,49 @@ export const NavItem = styled(Link)`
   color: #252525;
   text-decoration: none;
   transition: color 0.2s ease;
+  text-transform: uppercase;
 
   &:hover {
     color: #666;
   }
-`
+`;
+
+export const Burger = styled.div`
+    display: none;
+    flex-direction: column;
+    gap: 5px;
+    cursor: pointer;
+
+    div {
+        width: 25px;
+        height: 3px;
+        background: #252525;
+        border-radius: 2px;
+    }
+
+
+      @media (max-width: 767px) {
+        display: flex;
+  }
+`;
+
+export const MobileMenu = styled.div`
+    display: none;
+
+    @media (max-width: 767px) {
+        display: ${({$open}) => ($open ? "flex" : "none")};
+        position: absolute;
+        top: 90px;
+        left: 0;
+        width: 100%;
+        flex-direction: column;
+        align-items:center;
+        background-color: white;
+        box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+        padding: 20px 0;
+        gap: 15px;
+        z-index: 999;
+    }
+`;
 
 
