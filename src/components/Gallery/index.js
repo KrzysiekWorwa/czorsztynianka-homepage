6f32a193a9b3f3c1f240b51bcc8e0c68b94b32b3
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-23 14:48:27 +0200

    adding Lightbox to Gallery component and switching compressed photos

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import { GalleryGrid, GalleryHeader, GalleryImage, GalleryWrapper } from "./styled";
+import "yet-another-react-lightbox/styles.css";
+import Lightbox from "yet-another-react-lightbox";
 
-const Gallery = ({ title, images }) => (
-    <GalleryWrapper>
-        <GalleryHeader>{title}</GalleryHeader>
-        <GalleryGrid>
-            {images.map((img, index) => (
-                <GalleryImage key={index} src={img} loading="lazy" alt={`img ${index}`} />
-            ))}
-        </GalleryGrid>
-    </GalleryWrapper>
-);
+const Gallery = ({ title, images }) => {
+
+    const [index, setIndex] = useState(-1);
+
+    return (
+        <GalleryWrapper>
+            <GalleryHeader>{title}</GalleryHeader>
+            <GalleryGrid>
+                {images.map((img, index) => (
+                    <GalleryImage
+                        key={index}
+                        src={img}
+                        loading="lazy"
+                        alt={`img ${index}`}
+                        onClick={() => setIndex(index)}
+                    />
+                ))}
+            </GalleryGrid>
+
+            <Lightbox
+                open={index >= 0}
+                close={() => setIndex(-1)}
+                index={index}
+                slides={images.map((src) => ({ src }))}
+            />
+        </GalleryWrapper>
+    );
+};
 
 export default Gallery;
