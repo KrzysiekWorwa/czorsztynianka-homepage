d21f7ad39abbe717d4f81c86a44308be62fbc5ec
Krzysztof Worwa <worwak1998@gmail.com>
2025-05-28 23:23:51 +0200

    deleting react-router-dom package

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import Hero from './components/Hero';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import Partners from './components/Partners';
-import BikeRental from './components/BikeRental';
-import { HashRouter, Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <NavigationBar />
-      <Switch>
-        <Route exact path="/">
-          <Hero />
-          <Content />
-          <Partners />
-        </Route>
-        <Route path="/Wypożyczalnia-rowerów">
-          <BikeRental />
-        </Route>
-        <Route>
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+      <Hero />
+      <Content />
+      <Partners />
       <Footer />
-    </HashRouter>
+    </>
   );
 }
 export default App;
