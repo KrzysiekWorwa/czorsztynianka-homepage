be1ed71bd0273eb30d56ab0909c5e06d09f9fffe
Krzysztof Worwa <worwak1998@gmail.com>
2025-06-10 15:54:33 +0200

    add heroimage

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,14 @@
-import { HeroSection } from "./styled";
+import { HeroImage, HeroSection } from "./styled";
+import Hero1080 from "./baner-1080p.webp";
+import Hero1440 from "./baner-1440.webp";
 
 const Hero = () => (
-    <HeroSection id="home"/>
+    <HeroSection id="home">
+        <picture>
+            <source srcSet={Hero1440} media="(min-width: 1921px)" />
+            <HeroImage src={Hero1080} alt="Hero image" />
+        </picture>
+    </HeroSection>
 );
 
 export default Hero;
--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,22 +1,19 @@
 import styled from "styled-components";
-import BannerImage from "./New-logo.webp";
 
 export const HeroSection = styled.section`
   position: relative;
   height: 100vh;
   width: 100%;
-  background-image: url(${BannerImage});
-  background-size: cover;
- background-position: center;
-  background-repeat: no-repeat;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  text-align: center;
   padding-top: 90px;
 
       @media (max-width: 767px) {
         height: 50vh;
 }
+`;
+
+export const HeroImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
