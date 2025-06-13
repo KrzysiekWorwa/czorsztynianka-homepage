5ee091831e6372d287b64d122076b87c2aab3460
Krzysztof Worwa <worwak1998@gmail.com>
2025-06-13 21:12:30 +0200

    adding Houses index.js with houses content

--- src/components/Content/ContentSection/index.js
+++ src/components/Content/ContentSection/index.js
@@ -1,4 +1,5 @@
 import Cruises from "../Cruises";
+import Houses from "../Houses";
 import { Description, AddressLink, BookingIcon, ContactInfo, ContactSection, ContactWrapper, LinkWrapper, PhoneIcon, PinIcon, StatuteIcon, SubHeader } from "../styled";
 import {
     Section, Header, DescriptionSection, ImageWrapper, Image
@@ -26,37 +27,40 @@ const ContentSection = ({
                     {description}
                 </Description>
                 {linkHref && (
-                    <ContactWrapper>
-                        <ContactSection>
-                            <SubHeader>
-                                Dane kontaktowe
-                            </SubHeader>
-                            <ContactInfo>
-                                W celu rezerwacji oraz uzyskania dodatkowych informacji <br />
-                                zapraszamy do kontaktu telefonicznego lub na booking.com:
-                            </ContactInfo>
-                            <LinkWrapper>
-                                <AddressLink href="tel:+48509923730" title="telefon"><PhoneIcon /> 509 923 730</AddressLink>
-                            </LinkWrapper>
-                            <LinkWrapper>
-                                <AddressLink href={linkHref} target={linkTarget} rel={linkRel}><BookingIcon />{linkText}</AddressLink>
-                            </LinkWrapper>
-                            <LinkWrapper>
-                                <AddressLink href="https://zalewczorsztynski.com/wp-content/uploads/2019/07/wiatr_i_woda_regulamin.pdf" target="_blank" rel="noopener noreferrer"><StatuteIcon />Regulamin wynajmu</AddressLink>
-                            </LinkWrapper>
-                        </ContactSection>
-                        <ContactSection>
-                            <SubHeader>Adres</SubHeader>
-                            <ContactInfo>
-                                Domki "Zamkowe wzgórze" <br />
-                                Osiedle Zamek <br />
-                                34-441 Niedzica
+                    <>
+                        <Houses />
+                        <ContactWrapper>
+                            <ContactSection>
+                                <SubHeader>
+                                    Dane kontaktowe
+                                </SubHeader>
+                                <ContactInfo>
+                                    W celu rezerwacji oraz uzyskania dodatkowych informacji <br />
+                                    zapraszamy do kontaktu telefonicznego lub na booking.com:
+                                </ContactInfo>
                                 <LinkWrapper>
-                                    <AddressLink href="https://maps.app.goo.gl/HRPCRyEyQMwYhMgv5" target="_blank" rel="noopener noreferrer" title="Link do Google Maps"><PinIcon />Nawiguj z Google Maps</AddressLink>
+                                    <AddressLink href="tel:+48509923730" title="telefon"><PhoneIcon /> 509 923 730</AddressLink>
                                 </LinkWrapper>
-                            </ContactInfo>
-                        </ContactSection>
-                    </ContactWrapper>
+                                <LinkWrapper>
+                                    <AddressLink href={linkHref} target={linkTarget} rel={linkRel}><BookingIcon />{linkText}</AddressLink>
+                                </LinkWrapper>
+                                <LinkWrapper>
+                                    <AddressLink href="https://zalewczorsztynski.com/wp-content/uploads/2019/07/wiatr_i_woda_regulamin.pdf" target="_blank" rel="noopener noreferrer"><StatuteIcon />Regulamin wynajmu</AddressLink>
+                                </LinkWrapper>
+                            </ContactSection>
+                            <ContactSection>
+                                <SubHeader>Adres</SubHeader>
+                                <ContactInfo>
+                                    Domki "Zamkowe wzgórze" <br />
+                                    Osiedle Zamek <br />
+                                    34-441 Niedzica
+                                    <LinkWrapper>
+                                        <AddressLink href="https://maps.app.goo.gl/HRPCRyEyQMwYhMgv5" target="_blank" rel="noopener noreferrer" title="Link do Google Maps"><PinIcon />Nawiguj z Google Maps</AddressLink>
+                                    </LinkWrapper>
+                                </ContactInfo>
+                            </ContactSection>
+                        </ContactWrapper>
+                    </>
                 )}
                 {isCruiseSection && <Cruises />}
             </DescriptionSection>
--- src/components/Content/Houses/data.js
+++ src/components/Content/Houses/data.js
@@ -2,10 +2,8 @@ import houseImage from "./images/domek.webp"
 
 export const housesData = {
     id: "domki",
-    title: `Domki "Zamkowe wzgórze"`,
-    description: ` Oferujemy wynajem dwóch domków ulokowanych na brzegu Jeziora Czorsztyńskiego, tuż przy Zamku w Niedzicy.
-                Domki o metrażu 35 m kw, w pełni umeblowane, z wyposażoną kuchnią (lodówka, mikrofalówka, płyta indukcyjna, czajnik, naczynia do gotowania, zastawa stołowa dla 6 osób) i łazienką. Każdy z domków składa się z salonu z anksem kuchennym, dwóch sypialni i łazienki z prysznicem; posiada klimatyzację oraz WiFi. W salonie znajduje się telewizor. Do domków przynależą tarasy z meblami wypoczynkowymi.
-                Domek przeznaczony jest dla maksymalnie 6 osób.` ,
+    title: `Domki "Zamkowe wzgórze" w Niedzicy`,
+    description: ` Zapraszamy do odkrycia wyjątkowego miejsca na wypoczynek – naszych dwóch komfortowych domków, położonych w malowniczej scenerii Jeziora Czorsztyńskiego, tuż u stóp historycznego Zamku w Niedzicy. To idealne połączenie natury, historii i nowoczesnego komfortu, stworzone z myślą o niezapomnianych chwilach dla całej rodziny lub grupy przyjaciół.` ,
     imageSrc: houseImage,
     imageAlt: "Zdjęcie domku",
     linkHref: "https://www.booking.com/hotel/pl/zamkowe-wzgorze.pl.html",
