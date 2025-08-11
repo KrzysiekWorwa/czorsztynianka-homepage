1b677e209f046946a9d5867faa51ca0f01095ede
Krzysztof Worwa <worwak1998@gmail.com>
2025-08-11 20:54:04 +0200

    add animation to ContentSection component

--- src/components/Content/ContentSection/index.js
+++ src/components/Content/ContentSection/index.js
@@ -1,3 +1,4 @@
+import { StaggeredContainer, StaggeredItem } from "../../animations/staggeredFadeIn";
 import Bikes from "../Bikes";
 import Cruises from "../Cruises";
 import Houses from "../Houses";
@@ -23,7 +24,11 @@ const ContentSection = ({
 
     return (
         <Section id={id}>
-            <Header className="mobile-header">{title}</Header>
+            <StaggeredContainer>
+                <StaggeredItem>
+                    <Header className="mobile-header">{title}</Header>
+                </StaggeredItem>
+            </StaggeredContainer>
             <DescriptionSection>
                 <Description>
                     {description}
@@ -31,37 +36,41 @@ const ContentSection = ({
                 {linkHref && (
                     <>
                         <Houses />
-                        <ContactWrapper>
-                            <ContactSection>
-                                <SubHeader>
-                                    Dane kontaktowe
-                                </SubHeader>
-                                <ContactInfo>
-                                    W celu rezerwacji oraz uzyskania dodatkowych informacji <br />
-                                    zapraszamy do kontaktu telefonicznego lub na booking.com:
-                                </ContactInfo>
-                                <LinkWrapper>
-                                    <AddressLink href="tel:+48509923730" title="telefon"><PhoneIcon /> 509 923 730</AddressLink>
-                                </LinkWrapper>
-                                <LinkWrapper>
-                                    <AddressLink href={linkHref} target={linkTarget} rel={linkRel}><BookingIcon />{linkText}</AddressLink>
-                                </LinkWrapper>
-                                <LinkWrapper>
-                                    <AddressLink href="https://zalewczorsztynski.com/wp-content/uploads/2019/07/wiatr_i_woda_regulamin.pdf" target="_blank" rel="noopener noreferrer"><StatuteIcon />Regulamin wynajmu</AddressLink>
-                                </LinkWrapper>
-                            </ContactSection>
-                            <ContactSection>
-                                <SubHeader>Adres</SubHeader>
-                                <ContactInfo>
-                                    Domki "Zamkowe wzgórze" <br />
-                                    Osiedle Zamek <br />
-                                    34-441 Niedzica
-                                    <LinkWrapper>
-                                        <AddressLink href="https://maps.app.goo.gl/HRPCRyEyQMwYhMgv5" target="_blank" rel="noopener noreferrer" title="Link do Google Maps"><PinIcon />Nawiguj z Google Maps</AddressLink>
-                                    </LinkWrapper>
-                                </ContactInfo>
-                            </ContactSection>
-                        </ContactWrapper>
+                        <StaggeredContainer>
+                            <StaggeredItem>
+                                <ContactWrapper>
+                                    <ContactSection>
+                                        <SubHeader>
+                                            Dane kontaktowe
+                                        </SubHeader>
+                                        <ContactInfo>
+                                            W celu rezerwacji oraz uzyskania dodatkowych informacji <br />
+                                            zapraszamy do kontaktu telefonicznego lub na booking.com:
+                                        </ContactInfo>
+                                        <LinkWrapper>
+                                            <AddressLink href="tel:+48509923730" title="telefon"><PhoneIcon /> 509 923 730</AddressLink>
+                                        </LinkWrapper>
+                                        <LinkWrapper>
+                                            <AddressLink href={linkHref} target={linkTarget} rel={linkRel}><BookingIcon />{linkText}</AddressLink>
+                                        </LinkWrapper>
+                                        <LinkWrapper>
+                                            <AddressLink href="https://zalewczorsztynski.com/wp-content/uploads/2019/07/wiatr_i_woda_regulamin.pdf" target="_blank" rel="noopener noreferrer"><StatuteIcon />Regulamin wynajmu</AddressLink>
+                                        </LinkWrapper>
+                                    </ContactSection>
+                                    <ContactSection>
+                                        <SubHeader>Adres</SubHeader>
+                                        <ContactInfo>
+                                            Domki "Zamkowe wzgórze" <br />
+                                            Osiedle Zamek <br />
+                                            34-441 Niedzica
+                                            <LinkWrapper>
+                                                <AddressLink href="https://maps.app.goo.gl/HRPCRyEyQMwYhMgv5" target="_blank" rel="noopener noreferrer" title="Link do Google Maps"><PinIcon />Nawiguj z Google Maps</AddressLink>
+                                            </LinkWrapper>
+                                        </ContactInfo>
+                                    </ContactSection>
+                                </ContactWrapper>
+                            </StaggeredItem>
+                        </StaggeredContainer>
                     </>
                 )}
                 {isCruiseSection && <Cruises />}
