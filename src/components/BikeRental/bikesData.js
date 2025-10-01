9bf9c54a0be13cdca3bd82c2f3a57829edea4d6d
Krzysztof Worwa <worwak1998@gmail.com>
2025-10-01 15:53:05 +0200

    add new bike to bikerental component

--- src/components/BikeRental/bikesData.js
+++ src/components/BikeRental/bikesData.js
@@ -1,3 +1,4 @@
+import bikeImage from "./images/rower-cube-trapeze.webp"
 import bikeImage1 from "./images/rower-cube-533121-reaction-hybrid-performance-500.webp";
 import bikeImage2 from "./images/rower-elektryczny-cube-reaction-hybrid-pro-500-2256345-1080x636.webp";
 import bikeImage3 from "./images/hero-skl-focarini-petrol.webp";
@@ -8,57 +9,64 @@ import seatImage from "./images/polisport-fotelik.webp";
 import helmetImage from "./images/kask.webp";
 
 export const bikeList = [
-  {
+    {
     id: 1,
+    title: "Rower elektryczny Cube Reaction hybrid performance 625 Trapeze",
+    description: "Reaction Hybrid Performance 625 to idealne połączenie górskiej solidności z komfortem wspomagania elektrycznego. Kluczowa jest rama o profilu trapezowym, która ułatwia wsiadanie i zsiadanie, czyniąc rower wyjątkowo komfortowym i przyjaznym dla każdego. Rower idealnie pasuje do osób o różnym wzroście, w tym niższych użytkowników, gwarantując natychmiastowe poczucie bezpieczeństwa na trasie. Cichy i dynamiczny silnik Bosch Performance Line o momencie obrotowym 75 Nm w połączeniu z akumulatorem 625 Wh zapewnią moc i zasięg, które sprawią, że żaden cel nie będzie poza Twoim zasięgiem.",
+    price: "Cena: od 150 PLN",
+    image: bikeImage,
+  },
+  {
+    id: 2,
     title: "Rower elektryczny Cube Reaction hybrid performance",
     description: "Reaction Hybrid Performance korzysta z mocy silnika Bosch Performance Line, akumulatora 500 Wh i łatwej w obsłudze, 9-biegowej przekładni Shimano Cues. Mocne hydrauliczne hamulce tarczowe Shimano zapewniają niezawodną siłę hamowania w każdych warunkach pogodowych. Amortyzowany widelec o skoku 100 mm, wygodne siodełko i nieprzesuwające się chwyty typu lock-on zapewniają wygodę na długich trasach. ",
     price: "Cena: od 150 PLN",
     image: bikeImage1,
   },
   {
-    id: 2,
+    id: 3,
     title: "Rower elektryczny Cube Reaction hybrid pro",
     description: "Reaction Hybrid Pro 800 łączy wszechstronność roweru górskiego z łatwością użytkowania napędzaną silnikiem Bosch. Z łatwością pokonaj każdą trasę, w terenie lub na drodze, dzięki akumulatorowi 800 Wh i płynnie wspomagającemu pedałowanie, cichemu napędowi Bosch CX. W połączeniu z łatwą w obsłudze 12-biegową przekładnią Shimano XT to wszystko, czego potrzebujesz, aby wygodnie pokonać każde wzniesienie.",
     price: "Cena: od 150 PLN",
     image: bikeImage2,
   },
   {
-    id: 3,
+    id: 4,
     title: "Rower SKL Hero Focarini petrol",
     description: "To solidny rower dla osób poszukujących wyzwań podczas jazdy. Doskonałe połączenie niezawodności i jakości. Konstrukcja oparta na aluminiowej ramie oraz amortyzowanym widelcu aluminiowym z blokadą skoku. Zastosowanie lekkich aluminiowych komponentów znacząco wpływa na obniżenie wagi całego roweru a osprzęt Shimano gwarantuje płynną pracę komponentów napędu. Dzięki hamulcom tarczowym nie musisz obawiać się o skuteczność hamowania bez względu na warunki",
     price: "Cena: od 50 PLN",
     image: bikeImage3,
   },
   {
-    id: 4,
+    id: 5,
     title: "Rower SKL Invader",
     description: "To solidny rower dla osób ceniących sobie pewność prowadzenia i trwałość konstrukcji. Aluminiowa rama typu hardtail wykonana z lekkiego stopu gwarantuje wysoką sztywność i niską wagę. Amortyzowany widelec z możliwością blokady skutecznie tłumi nierówności, pozwalając na komfortową jazdę zarówno w terenie, jak i po asfalcie. Napęd oparty na komponentach Shimano oraz aluminiowym mechanizmie korbowym 3-rzędowym zapewnia precyzyjne i płynne przełożenia nawet pod obciążeniem. Mechaniczne hamulce tarczowe gwarantują pewne zatrzymanie niezależnie od warunków pogodowych.",
     price: "Cena: od 50 PLN",
     image: bikeImage4,
   },
   {
-    id: 5,
+    id: 6,
     title: "Rower SKL raptor",
     description: " Ten model to świetny wybór dla młodzieży lub niższych dorosłych, oferujący połączenie wygody, bezpieczeństwa i solidnych komponentów. Model oparty jest na solidnej aluminiowej ramie która zapewnia odpowiednią sztywność i łatwość manewrowania. Zastosowany amortyzowany widelec skutecznie tłumi drgania, co przekłada się na wyższy komfort jazdy, szczególnie na nierównym terenie. Napęd oparty na sprawdzonym zestawie Shimano pozwala na płynną zmianę biegów i dostosowanie tempa do ukształtowania terenu. Koła o rozmiarze 24” są idealne do miejskich tras, ścieżek rowerowych i lekkiego terenu. Zmiana przełożeń jest intuicyjna i szybka.",
     price: "Cena: od 50 PLN",
     image: bikeImage5,
   },
   {
-    id: 6,
+    id: 7,
     title: "Przyczepka Hamax Ixplorer",
     description: "Przeznaczona do wygodnego i bezpiecznego transportu dwójki dzieci podczas rowerowych przejażdżek i pieszych spacerów. Dziel z dziećmi przyjemność rowerowej przejażdżki lub pieszego spaceru. Dzięki okienkom, osłonie przeciwsłonecznej, osłonie przed deszczem i moskitierze dziecko w pełni cieszy się przejażdżką. Przyczepa posiada również 5-punktowe pasy dla zwiększenia bezpieczeństwa dziecka podczas jazdy. ",
     price: "Cena: 60 PLN",
     image: trailerImage,
   },
   {
-    id: 7,
+    id: 8,
     title: "Fotelik rowerowy Polisport Move",
     description: "Przeznaczony do wygodnego i bezpiecznego przewożenia dziecka na rowerze.  Dostosowany do jazdy dziecka w kasku. Wysokie ergonomiczne oparcie oraz specjalnie wyprofilowane podłokietniki gwarantują dziecku wygodną i naturalną pozycję, nawet podczas dłuższych tras a trzypunktowe pasy bezpieczeństwa umożliwiają szybkie i pewne zapięcie dziecka. Dodatkowe elementy, takie jak tylny odblask zwiększają bezpieczeństwo.  Fotelik przeznaczony jest dla dzieci o wadze od 9 do 22 kg.",
     price: "Cena: 30 PLN",
     image: seatImage,
   },
     {
-    id: 8,
+    id: 9,
     title: "Kask rowerowy",
     description: "Kask crossowy zaprojektowany z myślą o bezpieczeństwie i komforcie podczas jazdy na rowerze, hulajnodze czy rolkach. Dzięki 18 otworom wentylacyjnym i lekkiej konstrukcji doskonale odprowadza ciepło i wilgoć, zachowując najwyższe standardy ochrony. System regulacji Head Lock umożliwia idealne dopasowanie kasku do głowy użytkownika.",
     price: "Cena: 10 PLN",
